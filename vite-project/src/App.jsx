e982425c82f49671ceb9925f8094de55c7e98984
Larissa <98964013+larissa4p@users.noreply.github.com>
2022-09-13 15:23:28 -0300

    css global, components, api

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
+import {Outlet } from 'react-router-dom'
+import Navbar from "./components/Navbar";
+
 import './App.css'
+
 
 function App() {
 
-
   return (
     <div className="App">
+      <Navbar />
       <h2>Movies Lib</h2>
+      <Outlet />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
